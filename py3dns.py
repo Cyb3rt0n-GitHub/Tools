8c77e074142d4bdd7c2e91a3520bcb4822648701
Cyb3rt0n-GitHub <73040038+Cyb3rt0n-GitHub@users.noreply.github.com>
2023-08-08 18:55:57 +0100

    Update py3dns.py

--- py3dns.py
+++ py3dns.py
@@ -1,42 +1,41 @@
 import dns.resolver
 
-# Set the path to the input and output files
-input_file_path = "domains_new.txt"
-output_file_path = "domain_results.txt"
+def query_dns_record(domain, record_type):
+    try:
+        result = dns.resolver.resolve(domain, record_type)
+        return [str(record) for record in result]
+    except dns.resolver.NXDOMAIN:
+        return []
+    except Exception as e:
+        return [f"Error: {str(e)}"]
 
-# Open the input and output files
-with open(input_file_path, "r") as input_file, open(output_file_path, "w") as output_file:
-    # Loop through each line in the input file
-    for line in input_file:
-        # Get the domain name from the line and strip any whitespace
-        domain = line.strip()
+def main():
+    # Set the path to the input and output files
+    input_file_path = "domains.txt"
+    output_file_path = "domain_results.txt"
 
-        # Query for DMARC, DKIM, and SPF records
-        try:
-            dmarc_record = dns.resolver.query("_dmarc." + domain, "TXT")
-            dmarc = str(dmarc_record[0])
-        except:
-            dmarc = "No DMARC record found"
+    # Open the input and output files
+    with open(input_file_path, "r") as input_file, open(output_file_path, "w") as output_file:
+        # Loop through each line in the input file
+        for line in input_file:
+            # Get the domain name from the line and strip any whitespace
+            domain = line.strip()
 
-        try:
-            dkim_record = dns.resolver.query("default._domainkey." + domain, "TXT")
-            dkim = str(dkim_record[0])
-        except:
-            dkim = "No DKIM record found"
+            # Query for DMARC, DKIM, and SPF records
+            dmarc_records = query_dns_record("_dmarc." + domain, "TXT")
+            dmarc = "\n".join(dmarc_records) if dmarc_records else "No DMARC record found"
 
-        try:
-            spf_record = dns.resolver.query(domain, "TXT")
-            for record in spf_record:
-                if "v=spf1" in str(record):
-                    spf = str(record)
-                    break
-            else:
-                spf = "No SPF record found"
-        except:
-            spf = "No SPF record found"
+            dkim_records = query_dns_record("default._domainkey." + domain, "TXT")
+            dkim = "\n".join(dkim_records) if dkim_records else "No DKIM record found"
 
-        # Write the results to the output file
-        output_file.write(f"{domain}\n")
-        output_file.write(f"DMARC: {dmarc}\n")
-        output_file.write(f"DKIM: {dkim}\n")
-        output_file.write(f"SPF: {spf}\n\n")
+            spf_records = query_dns_record(domain, "TXT")
+            spf = next((str(record) for record in spf_records if "v=spf1" in str(record)), "No SPF record found")
+
+            # Write the results to the output file
+            output_file.write(f"{domain}\n")
+            output_file.write(f"DMARC:\n{dmarc}\n")
+            output_file.write(f"DKIM:\n{dkim}\n")
+            output_file.write(f"SPF:\n{spf}\n\n")
+
+if __name__ == "__main__":
+    main()
